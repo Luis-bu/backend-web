c8076fbb3de735958495ba75f940733c3df9913e
diazzz9 <danjuli110gmail.com>
2026-03-12 11:04:53 -0500

    feat: initial project setup

---
+++ src/main/java/co/reales/dw/backend_web/BackendWebApplication.java
@@ -0,0 +1,13 @@
+package co.reales.dw.backend_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/reales/dw/backend_web/BackendWebApplicationTests.java
@@ -0,0 +1,13 @@
+package co.reales.dw.backend_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
